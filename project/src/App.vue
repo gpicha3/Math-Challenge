4296473266f437535a50119b300ef616eca14fac
unknown <naphatsawan.bw@gmail.com>
2022-02-21 22:06:21 +0700

    editEnterforsubmit,settimeout

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,46 +1,47 @@
 <script setup>
 import { ref, computed } from "vue";
-
 //Random Number
-const randNumbers = ref("");
+const randNumbers = ref('');
+const randNumbers_backup = ref('');
 const generateNumber = () => {
   const num = Math.floor(Math.random() * 100);
   randNumbers.value = num;
+  randNumbers_backup.value = num;
+  setTimeout(()=>{randNumbers.value=' '} , 3000)
 };
 
 //Answer Input
-const ans = ref(' ')
+const ans = ref('')
 function check() {
-  if (ans.value == randNumbers.value) {
+  if (ans.value == randNumbers_backup.value) {
     generateNumber();
     ans.value = ''
   }
 }
-
 </script>
 
 <template>
   <div class="container">
-    <div class="title">
+    <div class="title" v-if="randNumbers ==``">
       Number Memory
       <div class="intro">The average person can remember 7 numbers at once. Can you do it more?</div>
       <div class="button">
         <button class="btn btn-warning btn-lg active" @click="generateNumber">Start</button>
-        <h1>{{ randNumbers }}</h1>
-        <!-- ** need correction ** -->
-        <input v-if="randNumbers != ``" v-model="ans" />
-        <button v-if="randNumbers != ``" @click="check">submit</button>
       </div>
     </div>
+    <h1>{{ randNumbers }}</h1>
+    <!-- ** need correction ** -->
+    <input v-if="randNumbers != ``" v-model="ans" @keyup.enter="check">
+    <button v-if="randNumbers != ``" @click="check">submit</button>
   </div>
 </template>
 
 <style>
 .container {
-  background-color: cornflowerblue;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+  text-align: center;
 }
 .title {
   font-size: 50px;
