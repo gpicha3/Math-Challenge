fa073444b0da3f5bb9b847f24029a7ca14b2cdcc
Wareewan Pongpunchaikul <68785361+wareewan-opal@users.noreply.github.com>
2022-02-22 21:07:32 +0700

    edit script and template

--- project/src/App.vue
+++ project/src/App.vue
@@ -4,25 +4,32 @@ import { ref, computed } from "vue";
 const randNumbers = ref('');
 const randNumbers_backup = ref('');
 const generateNumber = () => {
-  const num = Math.floor(Math.random() * 100);
+  const num = Math.floor(Math.random() * 10000);
   randNumbers.value = num;
   randNumbers_backup.value = num;
-  setTimeout(()=>{randNumbers.value=' '} , 3000)
+  setTimeout(() => { randNumbers.value = ' ' }, 4500)
 };
 
 //Answer Input
 const ans = ref('')
+
 function check() {
   if (ans.value == randNumbers_backup.value) {
     generateNumber();
     ans.value = ''
+  } else {
+    randNumbers.value = '';
+    ans.value = ''
   }
 }
+
+const load = 'play-animation'
+
 </script>
 
 <template>
   <div class="container">
-    <div class="title" v-if="randNumbers ==``">
+    <div class="title" v-if="randNumbers == ``">
       Number Memory
       <div class="intro">The average person can remember 7 numbers at once. Can you do it more?</div>
       <div class="button">
@@ -30,9 +37,13 @@ function check() {
       </div>
     </div>
     <h1>{{ randNumbers }}</h1>
-    <!-- ** need correction ** -->
-    <input v-if="randNumbers != ``" v-model="ans" @keyup.enter="check">
-    <button v-if="randNumbers != ``" @click="check">submit</button>
+    <input v-if="randNumbers != ``" v-model="ans" @keyup.enter="check" />
+    <button v-if="randNumbers != ``" @click="check" >submit</button>
+    <br>
+    <br>
+    <div class="progress-bar progress-bar-striped progress-bar-animated" v-if="randNumbers != ``" id="play-animation"></div>
+
+  
   </div>
 </template>
 
@@ -42,6 +53,8 @@ function check() {
   align-items: center;
   cursor: pointer;
   text-align: center;
+  height: 25px;
+  position: relative;
 }
 .title {
   font-size: 50px;
@@ -53,5 +66,24 @@ function check() {
 }
 .button {
   text-align: center;
+}
+.container .progress-bar {
+  position: absolute;
+  height: 100%;
+  border-radius: 7px;
+  background-color: greenyellow;
+}
+
+#play-animation {
+  animation: progress-animation 4.5s forwards;
+}
+
+@keyframes progress-animation {
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: 100%;
+  }
 }
 </style>
