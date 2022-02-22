d75c3419635233f3c8ddb17a5872d783b58da473
unknown <naphatsawan.bw@gmail.com>
2022-02-23 00:31:33 +0700

    add break page, summary page, check ans(next,back),css center

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,53 +1,93 @@
 <script setup>
 import { ref, computed } from "vue";
-//Random Number
+//Status Page
 const status = ref('none');
+const reset = ref(()=>{
+  status.value = 'none'
+  ans.value = ''
+  point.value = 0 
+});
+//Point Calculate
+let point = ref(0);
+
+//Random Number
 const showNumber = ref('');
 const respond = ref('');
 const generateNumber = () => {
-  const randNumbers = Math.floor(Math.random() * 10000);
+  const randNumbers = Math.floor(Math.random() * 100000 + 1000);
   status.value = 'ingame'
   showNumber.value = randNumbers;
   respond.value = randNumbers;
-  setTimeout(() => { showNumber.value = '';status.value="ans" }, 2500)
+  setTimeout(() => { showNumber.value = ''; status.value = 'ans' }, 2500)
+  ans.value = ''
 };
 
-//Answer Input
+//Checking Answer Input
 const ans = ref('')
+const ans_backup = ref('');
+
 function check() {
+  ans_backup.value = ans.value;
   if (ans.value == respond.value) {
-    generateNumber();
-    ans.value = ''
-  } else {
-    status.value = 'none';
+    point.value++;
+    status.value = 'break';
+    console.log('Point is :' + point.value);
+    return true;
+  }else{
+    status.value = 'back';
     showNumber.value = '';
+    console.log('Point is :' + point.value);
+    console.log(status.value);
+    return false;
   }
 }
-const load = 'play-animation'
+//Progress Bar
+const progress_bar = 'play-animation'
 
 </script>
 
 <template>
   <div class="container">
+
+    <!-- Menu Page -->
     <div class="title" v-if="status == `none`">
       Number Memory
-      <div class="intro">The average person can remember 7 numbers at once. Can you do it more?</div>
+      <div class="intro">
+        <h1>The average person can remember 7 numbers at once. Can you do it more?</h1>
+      </div>
       <div class="button">
         <button class="btn btn-warning btn-lg active" @click="generateNumber">Start</button>
       </div>
     </div>
-    <h1>{{ showNumber }}</h1>
-    <br>
+    <!-- ------ -->
+
+    <!-- inGame Page -->
+    <h1 v-show="status == `ingame`">{{ showNumber }}</h1>
+    <br />
     <div class="progress-bar" v-if="status == `ingame`" id="play-animation"></div>
-    <p v-show="status == `ans`">What was the number?</p>
-    <p v-show="status == `ans`">Press enter to submit</p>
-    <br>
+    <h2 v-show="status == `ans`">What was the number?</h2>
+    <h4 v-show="status == `ans`">Press enter to submit</h4>
     <input v-show="status == `ans`" v-model="ans" @keyup.enter="check" style="font-size: 50px; text-align: center;"/>
     <br>
     <br>
     <button class="btn btn-warning btn-lg active" v-if="status == `ans`" @click="check" style="font-size: 160%;">submit</button>
-    </div>
-    
+    <!-- ------ -->
+  
+    <!-- Break Page -->
+    <h2 v-show="status ==`break` ">Answer is : {{respond}}</h2>
+    <h2 v-show="status ==`break` ">Your answer is : {{ans_backup}}</h2>
+    <h3 v-show="status ==`break` ">Point : {{point}}</h3>
+    <br>
+    <button v-show="status ==`break`" class="btn btn-warning btn-lg active" @click="generateNumber">Next</button>
+    <!-- ------ --> 
+
+    <!-- Summary Page -->
+    <h2 v-show="status ==`back`">Nice Try ! </h2>
+    <h2 v-show="status ==`back`">Your points : {{point}}</h2>
+    <br>
+    <button v-show="status ==`back`" class="btn btn-warning btn-lg active" @click="reset" >Back to home</button>
+    <!-- ------ --> 
+  </div>
 </template>
 
 <style>
@@ -58,13 +98,13 @@ const load = 'play-animation'
   text-align: center;
   height: 15px;
   position: relative;
-  margin: 50px;
+
 }
 .title {
   font-size: 50px;
   text-align: center;
 }
-.intro {
+.intro h1 {
   font-size: 25px;
   text-align: center;
 }
@@ -91,10 +131,4 @@ const load = 'play-animation'
     width: 100%;
   }
 }
-p {
-  
-  font-size: 160%;
-  
-}
-
 </style>
